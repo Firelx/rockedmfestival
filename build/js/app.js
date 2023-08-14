a8fdc42d2e14e88f2d145482389dff99698820c0
Firelx <jgpe.lopezloera@gmail.com>
2023-08-13 22:07:11 -0600

    Agregar imagenes a galeria

--- build/js/app.js
+++ build/js/app.js
@@ -1,13 +1,35 @@
-document.addEventListener('DOMContentLoaded', function( ){
-    iniciarAPP();
+document.addEventListener('DOMContentLoaded', function () {
+	iniciarAPP();
 });
 
 function iniciarAPP() {
-    crearGaleria();
+	crearGaleria();
 }
 
 function crearGaleria() {
-    const galeria = document.querySelector('.galeria-imagenes');
+	const galeria = document.querySelector('.galeria-imagenes');
 
-    galeria.textContent = "Vamos a cambiar la galeria!";
+	for (let i = 1; i <= 12; i++) {
+		const imagen = document.createElement('picture');
+		imagen.innerHTML = `
+            <picture>
+            <source
+                srcset="build/img/thumb/${i}.avif"
+                type="image/avif"
+            />
+            <source
+                srcset="build/img/thumb/${i}.webp"
+                type="image/webp"
+            />
+            <img
+                loading="lazy"
+                width="300"
+                height="200"
+                src="img/thumb/${i}.jpg"
+                alt="Imagen galeria"
+            />
+            </picture>
+        `;
+		galeria.appendChild(imagen);
+	}
 }
